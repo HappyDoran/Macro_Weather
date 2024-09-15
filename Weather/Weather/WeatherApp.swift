8c532c6d63f8b3db857b1edd9699c4e9bdd8cbbc
Seo <kirin2211@naver.com>
2024-09-15 15:11:52 +0900

    [Feat]Project init

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Doran on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
